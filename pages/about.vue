5384b98088dcc1379eed41a420ccd090aabfbb0e
Matt Larson <matt.a.larson@gmail.com>
2018-01-24 07:42:39 -0600

    init commit

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+      <div class="container has-text-centered">
+          <h1 class="title">About me</h1>
+          <p>
+              My name is Matt Larson, I'm a (mostly) self taught software developer. 
+          </p>
+      </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Welcome to Saint Paul Soap</h1>
+      <h2 class="subtitle">home of Soapmaker Emily Dantuma.</h2>
+    </div>
+  </section>
+</template>
